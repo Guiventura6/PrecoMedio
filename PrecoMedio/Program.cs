7e8ef3c3ed4b2453d86cead3a578495e4f5278eb
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-10 20:31:02 -0300

    Implementacao da leitura do arquivo de entrada

--- PrecoMedio/Program.cs
+++ PrecoMedio/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using PrecoMedio.Entities;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace PrecoMedio
 {
@@ -6,7 +10,20 @@ namespace PrecoMedio
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using StreamReader sr = File.OpenText(path);
+
+            while (!sr.EndOfStream)
+            {
+                string[] fildes = sr.ReadLine().Split(",");
+                string name = fildes[0];
+                double price = double.Parse(fildes[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
         }
     }
 }

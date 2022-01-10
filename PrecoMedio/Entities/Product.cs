d6f3413bc8d17a906a823a648029bf36bfa91ae1
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-10 20:19:56 -0300

    Criacao da classe product.

---
+++ PrecoMedio/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace PrecoMedio.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
+

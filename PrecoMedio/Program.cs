ce6fc37accca3033255cd5339b5bfcc07278a909
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-10 21:07:11 -0300

    Implemtacao da saida da media de precos e produtos que estao abaixo desta media.

--- PrecoMedio/Program.cs
+++ PrecoMedio/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using PrecoMedio.Entities;
 using System.Globalization;
 using System.Collections.Generic;
@@ -23,6 +24,15 @@ namespace PrecoMedio
                 string name = fildes[0];
                 double price = double.Parse(fildes[1], CultureInfo.InvariantCulture);
                 list.Add(new Product(name, price));
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
             }
         }
     }
